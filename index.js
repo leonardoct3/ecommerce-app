a4761aa3b2f315a561a2d493d383bf3c6f4474b3
leonardoct3 <leocarvalhoteixeira@gmail.com>
2025-06-09 10:55:03 -0300

    chore: add package.json for ecommerce app with dependencies

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
-const express = require('express');
+import express from 'express';
+import { query } from './db/index.js';
 
 const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
     res.send("Hello World!");
+})
+
+app.get('/health', async (req, res) => {
+    const { rows } = await query('SELECT 1 AS ok');
+    res.json(rows[0]);
 })
 
 app.listen(port, () => {
